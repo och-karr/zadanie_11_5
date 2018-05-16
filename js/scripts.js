56d4255e06b07db99bc2dca6e247ffb42a7425e8
karolinneczka <okaa93@gmail.com>
2018-05-16 12:51:50 +0200

    Add basic files

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// function Phone(brand, price, color, weight, system) {  //funkcja konstruujaca
+// 	this.brand = brand;     //parametry
+// 	this.price = price;
+//     this.color = color;
+//     this.weight = weight;
+//     this.system = system;
+// }
+// Phone.prototype.printInfo = function() { // funkcja prototypowa - metoda printInfo
+// 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+// }
+
+// var iPhone6S = new Phone("Apple", 2250, "silver", 143, "iOS 10"); //obiekty na podstawie funkcji konstruujacej
+// var samsungGalaxyS6 = new Phone("Samsung", 1494, "black", 138, "Android");
+// var onePlusOne = new Phone("OnePlus", 1728, "white", 162, "iOS 10");
+
+// iPhone6S.printInfo(); //wywolanie metody printInfo dla obiektow
+// samsungGalaxyS6.printInfo();
+// onePlusOne.printInfo();
+
+// Phone.prototype.printMoreInfo = function() {
+// 	console.log("The phone weight is " + this.weight + "g and the system is " + this.system + ".");
+// }
+
+// iPhone6S.printMoreInfo();
+// samsungGalaxyS6.printMoreInfo();
+// onePlusOne.printMoreInfo();
+
