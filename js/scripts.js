f8d1c4a12892b3ae2c66a242602d8cafdbab5aa8
karolinneczka <okaa93@gmail.com>
2018-05-16 14:01:55 +0200

    Add class to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,21 @@
-// function Phone(brand, price, color, weight, system) {  //funkcja konstruujaca
-// 	this.brand = brand;     //parametry
-// 	this.price = price;
-//     this.color = color;
-//     this.weight = weight;
-//     this.system = system;
-// }
-// Phone.prototype.printInfo = function() { // funkcja prototypowa - metoda printInfo
-// 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-// }
+function Button(text) {
+	this.text = text || 'Hello'; //ustawiamy poczatkowa wartosc dla text w nowych obiektach, wlasciwosc dostaje domyslna wartosc Hello
+}
 
-// var iPhone6S = new Phone("Apple", 2250, "silver", 143, "iOS 10"); //obiekty na podstawie funkcji konstruujacej
-// var samsungGalaxyS6 = new Phone("Samsung", 1494, "black", 138, "Android");
-// var onePlusOne = new Phone("OnePlus", 1728, "white", 162, "iOS 10");
-
-// iPhone6S.printInfo(); //wywolanie metody printInfo dla obiektow
-// samsungGalaxyS6.printInfo();
-// onePlusOne.printInfo();
-
-// Phone.prototype.printMoreInfo = function() {
-// 	console.log("The phone weight is " + this.weight + "g and the system is " + this.system + ".");
-// }
-
-// iPhone6S.printMoreInfo();
-// samsungGalaxyS6.printMoreInfo();
-// onePlusOne.printMoreInfo();
+// prototyp - obsluguje przycisk na stronie
+Button.prototype = {
+	//metoda create tworzy przycisk na stronie
+	create: function() {
+		var self = this;
+	this.$element = $('<button>');  //Konstrukcja -this.$element- ma za zadanie trzymać w właściwości -element-
+									//przycisk (element DOM), który stworzyliśmy za pomocą jQuery.
+	this.$element.text(this.text);  // ustawiamy tekst na przycisku za pomoca jQuery
+	// ta metoda po kliknieciu w przycisk wyswietla tekst na ekranie
+	this.$element.click(function() {
+		alert(self.text);
+	});
+	$('body').append(this.$element); //bierzemy body i przypinamy do niego element
+	// this.$element.appendTo($('body')); --> ten sam efekt: pobieramy element i przypinamy go do body
+	}
+}
 
