9550637056f9fb9dd589fc1747f590a9b87c5aff
karolinneczka <okaa93@gmail.com>
2018-07-03 14:01:48 +0200

    Last corrections

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,16 @@ Button.prototype = {
 	//metoda create tworzy przycisk na stronie:
 	create: function() {
 		var self = this;
-	this.$element = $('<button>');  //Konstrukcja -this.$element- ma za zadanie trzymać w właściwości -element- >
-									//< przycisk (element DOM), który stworzyliśmy za pomocą jQuery.
-	this.$element.text(this.text);  // ustawiamy tekst na przycisku za pomoca jQuery
-	// ta metoda po kliknieciu w przycisk wyswietla tekst na ekranie:
-	this.$element.click(function() {
-		alert(self.text);
-	});
-	$('body').append(this.$element); //bierzemy body i przypinamy do niego element
-	// this.$element.appendTo($('body')); --> ten sam efekt: pobieramy element i przypinamy go do body
-	}
+		this.$element = $('<button>');  //Konstrukcja -this.$element- ma za zadanie trzymać w właściwości -element- >
+										//< przycisk (element DOM), który stworzyliśmy za pomocą jQuery.
+		this.$element.text(this.text);  // ustawiamy tekst na przycisku za pomoca jQuery
+		// ta metoda po kliknieciu w przycisk wyswietla tekst na ekranie:
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element); //bierzemy body i przypinamy do niego element
+		// this.$element.appendTo($('body')); --> ten sam efekt: pobieramy element i przypinamy go do body
+		}
 }
 
 var btn1 = new Button('Hello!'); //tworzenie pierwszej instancji klasy
